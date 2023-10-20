3cd640465e811b2fd8e3aee3cca1a7cb22e93e69
IdanArye <IdanArye@gmail.com>
2023-10-21 02:00:14 +0300

    Document `air_actions_tracking.rs`

--- src/control_helpers/air_actions_tracking.rs
+++ src/control_helpers/air_actions_tracking.rs
@@ -3,12 +3,18 @@ use bevy::prelude::*;
 use crate::controller::TnuaActionFlowStatus;
 use crate::prelude::*;
 
+/// An helper for tracking air actions.
+///
+/// It's [`update`](Self::update) must be called every frame - even when the result is not used.
+///
+/// For simpler usage, see [`TnuaSimpleAirActionsCounter`].
 #[derive(Default)]
 pub struct TnuaAirActionsTracker {
     considered_in_air: bool,
 }
 
 impl TnuaAirActionsTracker {
+    /// Call this every frame to track the air actions.
     pub fn update(&mut self, controller: &TnuaController) -> TnuaAirActionsUpdate {
         match controller.action_flow_status() {
             TnuaActionFlowStatus::NoAction => self.update_regardless_of_action(controller),
@@ -74,15 +80,28 @@ impl TnuaAirActionsTracker {
     }
 }
 
+/// The result of [`TnuaAirActionsTracker::update()`].
 #[derive(Debug, Clone, Copy)]
 pub enum TnuaAirActionsUpdate {
+    /// Nothing of interest happened this frame.
     NoChange,
+
+    /// The character has just started a free fall this frame.
     FreeFallStarted,
+
+    /// The character has just started an air action this frame.
     AirActionStarted(&'static str),
+
+    /// The character has just finished an air action this frame, and is still in the air.
     ActionFinishedInAir,
+
+    /// The character has just landed this frame.
     JustLanded,
 }
 
+/// A simple counter that counts together all the air actions a character is able to perform.
+///
+/// It's [`update`](Self::update) must be called every frame.
 #[derive(Component, Default)]
 pub struct TnuaSimpleAirActionsCounter {
     tracker: TnuaAirActionsTracker,
@@ -91,6 +110,7 @@ pub struct TnuaSimpleAirActionsCounter {
 }
 
 impl TnuaSimpleAirActionsCounter {
+    /// Call this every frame to track the air actions.
     pub fn update(&mut self, controller: &TnuaController) {
         let update = self.tracker.update(controller);
         match update {
@@ -114,6 +134,21 @@ impl TnuaSimpleAirActionsCounter {
         }
     }
 
+    /// Calculate the "air number" of an action.
+    ///
+    /// The air number of a ground action is 0. The first air jump (double jump) as an air number
+    /// of 1, the second (triple jump) has an air number of 2 and so on. Other air actions (like
+    /// air dashes) are counted together with the jumps.
+    ///
+    /// Use this number to:
+    /// 1. Determine if the action is allowed.
+    /// 2. Optionally change the action's parameters as the air number progresses.
+    ///
+    /// Note that the action name is important, because Tnua relies on constant feed of some
+    /// actions. As long as you pass the correct name, the number will not change while the action
+    /// continues to be fed. The correct name is [`TnuaAction::NAME`] when using
+    /// [`TnuaController::action`] or the first argument when using
+    /// [`TnuaController::named_action`].
     pub fn air_count_for(&self, action_name: &str) -> usize {
         if self.current_action == Some(action_name) {
             self.actions_including_current - 1
