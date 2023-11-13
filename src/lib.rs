3eec5cea425635f3f968ad17d10b467704a0ec23
Idan Arye <IdanArye@gmail.com>
2023-11-13 14:33:01 +0200

    Add documentation for the physics integration crates

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,27 @@
-//! # Tnua - A Character Controller for bevy_rapier.
+//! # Tnua - A Character Controller for Bevy.
 //!
 //! Tnua ("motion" in Hebrew) is a floating character controller, which means that instead of
 //! constantly touching the ground the character floats above it, which makes many aspects of the
 //! motion control simpler.
 //!
-//! Tnua uses [Rapier](https://rapier.rs/), and supports both the 2D and 3D versions of it with
-//! integration crates:
+//! Tnua can use [Rapier](https://rapier.rs/) or [XPBD](https://github.com/Jondolf/bevy_xpbd), and
+//! supports both the 2D and 3D versions of both with integration crates:
 //!
-//! * For 2D, add the `bevy-tnua-rapier2d` crate.
-//! * For 3D, add the `bevy-tnua-rapier3d` crate.
+//! * For Rapier 2D, add the [bevy-tnua-rapier2d](https://crates.io/crates/bevy-tnua-rapier2d) crate.
+//! * For Rapier 3D, add the [bevy-tnua-rapier3d](https://crates.io/crates/bevy-tnua-rapier3d) crate.
+//! * For XPBD 2D, add the [bevy-tnua-xpbd2d](https://crates.io/crates/bevy-tnua-xpbd2d) crate.
+//! * For XPBD 3D, add the [bevy-tnua-xpbd3d](https://crates.io/crates/bevy-tnua-xpbd3d) crate.
+//! * Third party integration crates. Such crates should depend on
+//!   [bevy-tnua-physics-integration-layer](https://crates.io/crates/bevy-tnua-physics-integration-layer)
+//!   and not the main bevy-tnua crate.
 //!
-//! In addition to the physics backend plugin, the
+//! Each physics integration crate has basic usage instructions for adding it in its documentation.
+//!
+//! In addition to the physics integration plugin, the
 //! [`TnuaControllerPlugin`](prelude::TnuaControllerPlugin) should also be added.
 //!
 //! A Tnua controlled character must have a dynamic rigid body, everything from
-//! `Tnua<physics-backend>IOBundle` (e.g. - for Rapier 2D, use `TnuaRapier2dIOBundle1), and
+//! `Tnua<physics-backend>IOBundle` (e.g. - for Rapier 3D, use `TnuaRapier3dIOBundle1), and
 //! everything from [`TnuaControllerBundle`](prelude::TnuaControllerBundle):
 //! ```no_run
 //! # use bevy::prelude::*;
@@ -36,8 +43,8 @@
 //!
 //! * Tnua, by default, casts a single ray to the ground. This can be a problem when the character
 //!   stands on a ledge, because the ray may be past the ledge while the character's collider
-//!   isn't. To avoid that, use `Tnua<physics-backend>SensorShape` (e.g. - for Rapier 2D, use
-//!   `TnuaRapier2dSensorShape`) to replace the ray with a shape that resembles the collider. It is
+//!   isn't. To avoid that, use `Tnua<physics-backend>SensorShape` (e.g. - for Rapier 3D, use
+//!   `TnuaRapier3dSensorShape`) to replace the ray with a shape that resembles the collider. It is
 //!   better to use a shape a little bit smaller than the collider, so that when the character
 //!   presses against a wall Tnua won't think it should be lifted up when the casted shape hits
 //!   that wall.
