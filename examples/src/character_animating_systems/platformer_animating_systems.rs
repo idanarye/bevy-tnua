203c68139843de20295c3f1a9561ad84e3a514a7
Idan Arye <IdanArye@gmail.com>
2024-01-18 17:55:26 +0200

    Add comments for explaining the animating system in the example

--- examples/src/character_animating_systems/platformer_animating_systems.rs
+++ examples/src/character_animating_systems/platformer_animating_systems.rs
@@ -18,7 +18,15 @@ pub enum AnimationState {
 
 pub fn animate_platformer_character(
     mut animations_handlers_query: Query<(
+        // `TnuaAnimatingState` is a helper for controlling the animations. The user system is
+        // expected to provide it with an enum on every frame that describes the state of the
+        // character. The helper then tells the user system if the enum variant changed - which
+        // usually means the system should start a new animation - or remained the same, which
+        // means that the system should not change the animation (but maybe change its speed based
+        // on the enum's payload)
         &mut TnuaAnimatingState<AnimationState>,
+        // The controller can be used to determine the state of the character - information crucial
+        // for deciding which animation to play.
         &TnuaController,
         &AnimationsHandler,
     )>,
@@ -28,12 +36,23 @@ pub fn animate_platformer_character(
         let Ok(mut player) = animation_players_query.get_mut(handler.player_entity) else {
             continue;
         };
+        // We need to determine the animating status of the character on each frame, and feed it to
+        // `update_by_discriminant` which will decide whether or not we need to switch the
+        // animation.
         match animating_state.update_by_discriminant({
+            // We use the action name because it's faster than trying to cast into each action
+            // type. We'd still have to cast into the action type later though, to get
+            // action-specific data.
             match controller.action_name() {
+                // For builtin actions, prefer using the `NAME` const from the `TnuaAction` trait.
                 Some(TnuaBuiltinJump::NAME) => {
+                    // In case of jump, we want to cast it so that we can get the concrete jump
+                    // state.
                     let (_, jump_state) = controller
                         .concrete_action::<TnuaBuiltinJump>()
                         .expect("action name mismatch");
+                    // Depending on the state of the jump, we need to decide if we want to play the
+                    // jump animation or the fall animation.
                     match jump_state {
                         TnuaBuiltinJumpState::NoJump => continue,
                         TnuaBuiltinJumpState::StartingJump { .. } => AnimationState::Jumping,
@@ -46,6 +65,8 @@ pub fn animate_platformer_character(
                     }
                 }
                 Some(TnuaBuiltinCrouch::NAME) => {
+                    // In case of crouch, we need the state of the basis to determine - based on
+                    // the speed - if the charcter is just crouching or also crawling.
                     let Some((_, basis_state)) = controller.concrete_basis::<TnuaBuiltinWalk>()
                     else {
                         continue;
@@ -60,9 +81,13 @@ pub fn animate_platformer_character(
                         (Some(speed), true) => AnimationState::Crawling(0.1 * speed),
                     }
                 }
+                // For the dash, we don't need the internal state of the dash action to determine
+                // the action - so there is no need to downcast.
                 Some(TnuaBuiltinDash::NAME) => AnimationState::Dashing,
                 Some(other) => panic!("Unknown action {other}"),
                 None => {
+                    // If there is no action going on, we'll base the animation on the state of the
+                    // basis.
                     let Some((_, basis_state)) = controller.concrete_basis::<TnuaBuiltinWalk>()
                     else {
                         continue;
@@ -80,17 +105,30 @@ pub fn animate_platformer_character(
                 }
             }
         }) {
+            // `Maintain` means that the same animation state continues from the previous frame, so
+            // we shouldn't switch the animation.
             TnuaAnimatingStateDirective::Maintain { state } => match state {
+                // Some animation states have parameters, that we may want to use to control the
+                // animation (without necessarily replacing it). In this case - control the speed
+                // of the animation based on the speed of the movement.
                 AnimationState::Running(speed) | AnimationState::Crawling(speed) => {
                     player.set_speed(*speed);
                 }
+                // Jumping and dashing can be chained, we want to start a new jump/dash animation
+                // when one jump/dash is chained to another.
                 AnimationState::Jumping | AnimationState::Dashing => {
                     if controller.action_flow_status().just_starting().is_some() {
                         player.seek_to(0.0);
                     }
                 }
+                // For other animations we don't have anything special to do - so we just let them
+                // continue.
                 _ => {}
             },
+            // `Alter` means that the character animation state has changed, and thus we need to
+            // start a new animation. The actual implementation for each possible animation state
+            // is straightforward - we start the animation, set its speed if the state has a
+            // variable speed, and set it to repeat if it's something that needs to repeat.
             TnuaAnimatingStateDirective::Alter {
                 old_state: _,
                 state,
