ead1da1690213f60711d1364acd40300d5c35d19
person93 <person93.person93@gmail.com>
2025-01-14 10:30:05 -0500

    Deprecate rapier IO bundles

--- rapier2d/src/lib.rs
+++ rapier2d/src/lib.rs
@@ -47,6 +47,9 @@ impl Default for TnuaRapier2dPlugin {
 
 impl Plugin for TnuaRapier2dPlugin {
     fn build(&self, app: &mut App) {
+        app.register_required_components::<TnuaProximitySensor, Velocity>()
+            .register_required_components::<TnuaProximitySensor, ExternalForce>()
+            .register_required_components::<TnuaProximitySensor, ReadMassProperties>();
         app.configure_sets(
             self.schedule,
             TnuaSystemSet.before(PhysicsSet::SyncBackend).run_if(
@@ -70,6 +73,9 @@ impl Plugin for TnuaRapier2dPlugin {
 
 /// `bevy_rapier2d`-specific components required for Tnua to work.
 #[derive(Bundle, Default)]
+#[deprecated(
+    note = "All uses can be safely removed, components are added via bevy's required components"
+)]
 pub struct TnuaRapier2dIOBundle {
     pub velocity: Velocity,
     pub external_force: ExternalForce,
