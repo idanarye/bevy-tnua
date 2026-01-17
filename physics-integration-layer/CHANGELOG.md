b43d4b7b2bff94ae30fed9eaba7e6c49b6628c6c
Idan Arye <IdanArye@gmail.com>
2026-01-18 00:07:58 +0200

    Upgrade Bevy to 0.18

--- physics-integration-layer/CHANGELOG.md
+++ physics-integration-layer/CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Changed
+- Upgrade to Bevy 0.18.
 
 ## 0.11.0 - 2026-01-03
 ### Added
--- src/basis_action_traits.rs
+++ src/basis_action_traits.rs
@@ -84,9 +84,9 @@ pub trait TnuaSchemeConfig: Serialize + for<'de> Deserialize<'de> {
     /// on every run and to avoid breaking WASM (which cannot access the assets directory using the
     /// filesystem)
     fn write_if_not_exist(&self, path: impl AsRef<Path>) -> std::io::Result<()> {
-        let serialized = bevy::asset::ron::ser::to_string_pretty(
+        let serialized = ron::ser::to_string_pretty(
             self,
-            bevy::asset::ron::ser::PrettyConfig::new(),
+            ron::ser::PrettyConfig::new(),
         )
         .expect("Should be able to serialize all configs to RON");
         let file = File::options().write(true).create_new(true).open(path);
