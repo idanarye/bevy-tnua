97fd3ef7ecfb66a51d7f279778246e1ebc973946
Idan Arye <IdanArye@gmail.com>
2026-04-02 23:55:13 +0300

    Fix #123: manually enforce hit order in Avian ray/shape casts

--- demos/src/levels_setup/close_platforms_2d.rs
+++ demos/src/levels_setup/close_platforms_2d.rs
@@ -1,6 +1,6 @@
 use bevy::{color::palettes::css, prelude::*};
 use bevy_tnua::TnuaGhostPlatform;
-use bevy_tnua::math::{Float, Vector2};
+use bevy_tnua::math::Vector2;
 
 #[cfg(feature = "avian2d")]
 use avian2d::prelude::*;
@@ -17,26 +17,31 @@ pub fn setup_level(mut helper: LevelSetupHelper2d) {
         helper.spawn_rectangle(
             format!("Floor {i}"),
             css::BROWN,
-            Transform::from_xyz(0.0, i as Float * 0.4, 0.0),
-            Vector2::new(8.0, 0.1)
+            Transform::from_xyz(0.0, i as f32 * 0.4, 0.0),
+            Vector2::new(8.0, 0.1),
         );
     }
 
     for i in 0..4 {
-        helper.spawn_rectangle(
-            format!("Ghost {i}"),
-            css::ORANGE,
-            Transform::from_xyz(6.0, 0.2 + i as Float * 0.4, 0.0),
-            Vector2::new(8.0, 0.1)
-        ).insert((
-            #[cfg(feature = "rapier2d")]
-            SolverGroups {
-                memberships: Group::empty(),
-                filters: Group::empty(),
-            },
-            #[cfg(feature = "avian2d")]
-            CollisionLayers::new([super::for_2d_platformer::LayerNames::FallThrough], [super::for_2d_platformer::LayerNames::FallThrough]),
-            TnuaGhostPlatform,
-        ));
+        helper
+            .spawn_rectangle(
+                format!("Ghost {i}"),
+                css::ORANGE,
+                Transform::from_xyz(6.0, 0.2 + i as f32 * 0.4, 0.0),
+                Vector2::new(8.0, 0.1),
+            )
+            .insert((
+                #[cfg(feature = "rapier2d")]
+                SolverGroups {
+                    memberships: Group::empty(),
+                    filters: Group::empty(),
+                },
+                #[cfg(feature = "avian2d")]
+                CollisionLayers::new(
+                    [super::for_2d_platformer::LayerNames::FallThrough],
+                    [super::for_2d_platformer::LayerNames::FallThrough],
+                ),
+                TnuaGhostPlatform,
+            ));
     }
 }
--- demos/src/levels_setup/close_platforms_3d.rs
+++ demos/src/levels_setup/close_platforms_3d.rs
@@ -1,6 +1,6 @@
 use bevy::{color::palettes::css, prelude::*};
 use bevy_tnua::TnuaGhostPlatform;
-use bevy_tnua::math::{Float, Vector3};
+use bevy_tnua::math::Vector3;
 
 #[cfg(feature = "avian3d")]
 use avian3d::prelude::*;
@@ -17,26 +17,31 @@ pub fn setup_level(mut helper: LevelSetupHelper3d) {
     for i in 0..4 {
         platforms_helper.spawn_cuboid(
             format!("Floor {i}"),
-            Transform::from_xyz(0.0, i as Float * 0.4, 0.0),
-            Vector3::new(8.0, 0.1, 2.0)
+            Transform::from_xyz(0.0, i as f32 * 0.4, 0.0),
+            Vector3::new(8.0, 0.1, 2.0),
         );
     }
 
     let mut ghost_platforms_helper = helper.with_color(css::ORANGE);
     for i in 0..4 {
-        ghost_platforms_helper.spawn_cuboid(
-            format!("Ghost {i}"),
-            Transform::from_xyz(6.0, 0.2 + i as Float * 0.4, 0.0),
-            Vector3::new(8.0, 0.1, 2.0)
-        ).insert((
-            #[cfg(feature = "rapier3d")]
-            SolverGroups {
-                memberships: Group::empty(),
-                filters: Group::empty(),
-            },
-            #[cfg(feature = "avian3d")]
-            CollisionLayers::new([super::for_3d_platformer::LayerNames::FallThrough], [super::for_3d_platformer::LayerNames::FallThrough]),
-            TnuaGhostPlatform,
-        ));
+        ghost_platforms_helper
+            .spawn_cuboid(
+                format!("Ghost {i}"),
+                Transform::from_xyz(6.0, 0.2 + i as f32 * 0.4, 0.0),
+                Vector3::new(8.0, 0.1, 2.0),
+            )
+            .insert((
+                #[cfg(feature = "rapier3d")]
+                SolverGroups {
+                    memberships: Group::empty(),
+                    filters: Group::empty(),
+                },
+                #[cfg(feature = "avian3d")]
+                CollisionLayers::new(
+                    [super::for_3d_platformer::LayerNames::FallThrough],
+                    [super::for_3d_platformer::LayerNames::FallThrough],
+                ),
+                TnuaGhostPlatform,
+            ));
     }
 }
